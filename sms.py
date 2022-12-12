51bb5fb1710fa849aeb0b9b1522f995ee0f6aef7
Teknoist <37031361+Teknoist@users.noreply.github.com>
2022-12-12 12:05:34 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,25 +2,17 @@ import subprocess
 import random
 import string
 
-# rasgele 7 karakterli bir dize oluştur
-letters = string.ascii_uppercase
-sms = ( ''.join(random.choice(letters) for i in range(7)) )
-
-
-# SMS Message Template (try to keep to within 150 characters)
-smsmessage = ("BAGLAN4GB " + sms)
 
 # Kullanıcıdan kaç kere tekrar edileceğini sor
 tekrar_sayisi = int(input("Kaç kere tekrar edilsin? "))
 
 # Belirtilen sayıda tekrar edilen döngü
 for i in range(tekrar_sayisi):
-# Use Subprocess Run Function to send SMS
-subprocess.run(["termux-sms-send", "-n", "2200", "-s", "1", smsmessage])
-# Print confirmation of each send
-print(smsmessage)
-
-
-
-
-
+  # rasgele 7 karakterli bir dize oluştur
+  letters = string.ascii_uppercase
+  sms = ( ''.join(random.choice(letters) for i in range(7)) )
+  smsmessage = ("BAGLAN4GB " + sms)
+  # Use Subprocess Run Function to send SMS
+  subprocess.run(["termux-sms-send", "-n", "2200", "-s", "1", smsmessage])
+  # Print confirmation of each send
+  print(smsmessage)
