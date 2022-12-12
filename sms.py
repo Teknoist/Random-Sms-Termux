9d22a2e4e983e933b8640318dc8100e6c6b70f1b
Teknoist <37031361+Teknoist@users.noreply.github.com>
2022-12-12 11:59:34 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -10,15 +10,15 @@ sms = ( ''.join(random.choice(letters) for i in range(7)) )
 # SMS Message Template (try to keep to within 150 characters)
 smsmessage = ("BAGLAN4GB " + sms)
 
-# Use Subprocess Run Function to send SMS
-subprocess.run(["termux-sms-send", "-n", "2200", smsmessage])
+# Kullanıcıdan kaç kere tekrar edileceğini sor
+tekrar_sayisi = int(input("Kaç kere tekrar edilsin? "))
 
+# Belirtilen sayıda tekrar edilen döngü
+for i in range(tekrar_sayisi):
+# Use Subprocess Run Function to send SMS
+subprocess.run(["termux-sms-send", "-n", "2200", "-s", "1", smsmessage])
 # Print confirmation of each send
 print(smsmessage)
-
-
-# Print end of process message
-print("İşlem tamamlandı.")
 
 
 
